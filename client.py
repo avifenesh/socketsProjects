65dd2e913eec24871441f02ed786f87c2ae0e643
Avi Fenesh <aviarchi1994@gmail.com>
2020-09-20 16:15:17 +0300

    make my first server, and make the server and client to comunicate

--- client.py
+++ client.py
@@ -1,11 +1,13 @@
 import socket
 
+
 my_socket = socket.socket()
 
-my_socket.connect(('127.0.0.1', 1729))
-my_socket.send('Avi')
+my_socket.connect(('127.0.0.1', 8820))
+name = input("please type your name here - ").encode()
+my_socket.send(name)
 
 data = my_socket.recv(1024)
-print("The server sent: " + data)
+print("The server sent: " + data.decode())
 
 my_socket.close()
