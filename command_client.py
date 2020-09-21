2b1c28d954afaf664a714f7b0c40c6243103962a
Avi Fenesh <aviarchi1994@gmail.com>
2020-09-21 23:53:17 +0300

    maked a bit order withe the variables

--- command_client.py
+++ command_client.py
@@ -5,7 +5,6 @@ my_socket = socket.socket()
 my_socket.connect(('127.0.0.1', 8820))
 command = input("use one of those commands - TIME, NAME, RAND, EXIT.  please type your command here - ")
 my_socket.send(command.encode())
-
 data = my_socket.recv(1024)
 print("The server sent: " + data.decode())
 
